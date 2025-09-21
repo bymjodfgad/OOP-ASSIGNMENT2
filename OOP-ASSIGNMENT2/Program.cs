e864a10dfbf7b83c214eb020c05932722f5913fd
bymjodfgad <bymjodfgad@gmail.com>
2025-09-21 08:36:44 +0200

    Fixed program to print the classes ive made.

--- OOP-ASSIGNMENT2/Program.cs
+++ OOP-ASSIGNMENT2/Program.cs
@@ -7,6 +7,14 @@
             //cinema
             Cinema cinema = new Cinema("Trollhättans Biograf", true); //Name and if its open or not. true==open.
             cinema.DisplayInfo();
+
+            Customer customer = new Customer(1, "Jacob", "jacob@mail.com");
+            customer.AddPoints(5);
+
+            Employee employee = new Employee(2, "Adam", "adam@mail.com", "Owner");
+            employee.AssignTask("Count money in register");
+            Console.WriteLine(employee.getName());
+            Console.WriteLine(customer.getName());
         }
     }
 }
